bfabcf249490a35a476dd4ec826cfa8c74a8f14b
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2023-02-03 01:26:17 -0500

    feat(medium): trim

---
+++ src/medium/00108-medium-trim.ts
@@ -0,0 +1,30 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Trim<"str">, "str">>,
+  Expect<Equal<Trim<" str">, "str">>,
+  Expect<Equal<Trim<"     str">, "str">>,
+  Expect<Equal<Trim<"str   ">, "str">>,
+  Expect<Equal<Trim<"     str     ">, "str">>,
+  Expect<Equal<Trim<"   \n\t foo bar \t">, "foo bar">>,
+  Expect<Equal<Trim<"">, "">>,
+  Expect<Equal<Trim<" \n\t ">, "">>
+];
+
+// ============= Your Code Here =============
+/**
+ * I could've also used TrimLeft and TrimRight,
+ * but at the time I don't have TrimRight and
+ * I kinda want to do it bare bones.
+ * It would've been something like this
+ * @example
+ * TrimRight<TrimLeft<S>>
+ */
+type Trim<S extends string> = S extends
+  | `${TrimChars}${infer M}`
+  | `${infer M}${TrimChars}`
+  ? Trim<M>
+  : S;
+
+type TrimChars = " " | "\t" | "\n";
